11f4cf3bc9a5fddb3a3a1274e3b3e1a1e2d3b63a
dlgkdud1118 <110403139+dlgkdud1118@users.noreply.github.com>
2024-04-29 21:22:59 +0900

    Add files via upload

---
+++ 2024.04.29_java_tutorials_zyannkenn/src/GameResult.java
@@ -0,0 +1,69 @@
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class GameResult implements Serializable{
+
+	private static int nextNo = 1;
+	public static int getNextNo() { 
+		return nextNo;
+	}
+	public static void setNextNo(int nextNo) {
+		GameResult.nextNo = nextNo;
+	}
+	
+	private int no;
+	private Date gameDate;
+	private int comNo;
+	private int userNo;
+	private String result;
+	
+	public GameResult() { 
+		this.no = nextNo;
+		nextNo++;
+	}
+	public GameResult(int no, Date gameDate, int comNo, int userNo, String result) {
+		this();
+		this.gameDate = gameDate;
+		this.comNo = comNo;
+		this.userNo = userNo;
+		this.result = result;
+	}
+	
+	public int getNo() { 
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public Date getGameDate() {
+		return gameDate;
+	}
+	public int getComNo() { 
+		return comNo;
+	}
+	public void setComNo(int comNo) {
+		this.comNo = comNo;
+	}
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	@Override
+	public String toString() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd a hh:mm:ss");
+		return String.format("[%05d][%s][%d][%d][%s]",no, sdf.format(gameDate), comNo, userNo, result);
+	}
+	
+	
+}
---
+++ 2024.04.29_java_tutorials_zyannkenn/src/TheGame.java
@@ -0,0 +1,119 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class TheGame{
+
+	private java.util.Scanner scanner = new java.util.Scanner(System.in);
+	
+	private ArrayList<String> gameResults = new ArrayList<>();
+	private ArrayList<GameResult> gameResults2 = new ArrayList<>();
+	
+	public void doGame() {
+		outer: while(true) {
+			String selection = selectMenu();
+			
+			System.out.println();
+			
+			switch(selection) {
+			case "1":
+				int comNumber = makeComNumber();
+					
+					int userNumber = inputUserNumber();
+					
+					String result = makeDecision(userNumber, comNumber);
+					
+					showResult(comNumber, userNumber, result);
+			
+					gameResults.add(result); //결과 저장
+					GameResult gr = new GameResult(0, new Date(), comNumber, userNumber, result);
+					gameResults2.add(gr);
+					break;
+			case "2":
+				for (String gameResult : gameResults) {
+					System.out.println(gameResult);
+				}
+				for(GameResult gameResult : gameResults2) {
+					System.out.println(gameResult);
+				}
+				break;
+				
+			case "9":
+				System.out.println("**** 終了 ****");
+				break outer;
+			default:
+				System.out.println("**** 対応していない命令です。***");
+				break;
+			}
+			System.out.println();
+		}
+		scanner.close();
+	}
+
+	public String selectMenu() {
+		System.out.println("-----------------------------------");
+		System.out.println("1. じゃん拳ゲーム開始");
+		System.out.println("2. ゲーム結果");
+		System.out.println("9. 終了 ");
+		System.out.println("-----------------------------------");
+		System.out.print("ジョブを選択してください : ");
+		String selection = scanner.next();
+		
+		return selection;
+	}
+	
+	public int makeComNumber() {
+		double n = Math.random()*3;
+		int comNumber = (int)(n + 1);
+		return comNumber;
+	}
+
+	public int inputUserNumber() {
+		String sUserNumber;
+		do {
+				System.out.print("チョキン/ぐっと/ぱっと 入力 (チョキン=1, ぐっと＝2, ぱっと=3 : ");
+				sUserNumber = scanner.next();
+		} while (!sUserNumber.equals("1") &&
+						!sUserNumber.equals("2") &&
+						!sUserNumber.equals("3"));
+		int userNumber = Integer.parseInt(sUserNumber);
+		return userNumber;
+	}
+
+	public String makeDecision(int userNumber, int comNumber) { // 게임결과
+		String result = "DRAW";
+		if ((userNumber == 1 && comNumber == 2) ||
+			(userNumber == 2 && comNumber == 3) ||
+			(userNumber == 3 && comNumber == 1)	) {
+			result = "LOSE";
+		} else if (userNumber == comNumber) {
+		} else {
+			result = "WIN";
+		}
+		return result;
+	}
+
+	
+	public void showResult(int comNumber, int userNumber, String result) {
+		String com;
+		if (comNumber == 1)		com = "チョキン";
+		else if(comNumber ==2)  com = "ぐっと";
+		else					com = "ぱっと";
+		String user = userNumber == 1 ? "チョキン" : (userNumber == 2 ? "ぐっと" : "ぱっと");
+		System.out.printf("[COMPUTER : %2s]　[USER : %2s]　[RESULT : %s]\n", com, user, result);
+	}
+	
+	public static void main(String[] args) {
+		TheGame game = new TheGame();
+		game.doGame();
+	}
+
+
+
+
+
+
+
+
+	
+	
+}
